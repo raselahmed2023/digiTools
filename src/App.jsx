89aad1a3cea3482c10b28be424a6effb44832b16
Rasel Ahmed <raselahmediu22@gmail.com>
2026-03-30 15:11:24 +0600

    cards fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
-
 import './App.css'
+import Models from './Components/Models'
 import Navbar from './Components/Navbar'
+
+const getModels = async () => {
+const res=await fetch("/models.json")
+return res.json()
+}
+
+const modelPromise=getModels()
 
 
 function App() {
 
   return (
     <>
-   <Navbar></Navbar>
+      <Navbar></Navbar>
+
+      <Models  modelPromise={modelPromise}></Models>
 
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                             <li><a>FAQ</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-3xl text-[#9514FA]">DigiTools</a>
+                    <a className="text-3xl text-[#9514FA]">DigiTools</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -33,7 +33,7 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end gap-5">
                     <a href="">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
                         </a>
                     <a className='text-xl'>Login</a>
                     <a className="btn rounded-full p-4 text-white bg-[#9514FA]">Get Started</a>
