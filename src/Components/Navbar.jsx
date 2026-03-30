45acd9666d547c4913f7079d7dbee0063363e8ad
Rasel Ahmed <raselahmediu22@gmail.com>
2026-03-30 09:27:24 +0600

    navbar is done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='navbar bg-base-100 shadow px-4' >
+
+            <div className="navbar container mx-auto ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a>Products</a></li>
+                            <li><a>Features</a></li>
+                            <li><a>Pricing</a></li>
+                            <li><a>Testimonials</a></li>
+                            <li><a>FAQ</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-3xl text-[#9514FA]">DigiTools</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a className='text-xl'>Products</a></li>
+                        <li><a className='text-xl'>Features</a></li>
+                        <li><a className='text-xl'>Pricing</a></li>
+                        <li><a className='text-xl'>Testimonials</a></li>
+                        <li><a className='text-xl'>FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end gap-5">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                        </a>
+                    <a className='text-xl'>Login</a>
+                    <a className="btn rounded-full p-4 text-white bg-[#9514FA]">Get Started</a>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
