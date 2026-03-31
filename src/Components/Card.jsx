9419bf42c21307268f27c0131a9a72885993e785
Rasel Ahmed <raselahmediu22@gmail.com>
2026-03-31 16:56:13 +0600

    README file and key is Updated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -35,7 +35,7 @@ const Card = ({ model, carts, setCarts }) => {
                     </div>
                     <ul className="mt-2 flex flex-col gap-2 text-sm">
                         {model.features.map((feature, i) => (
-                            <li key={i.id}>
+                            <li key={i}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24"
                                     stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                                 <span>{feature}</span>
